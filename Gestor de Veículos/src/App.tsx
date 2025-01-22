3179d160a619777eeacff74f3a21bf98587c562d
Carlos Vinicius <carlos09v@gmail.com>
2025-01-22 16:51:59 -0300

    Config do AppRouter + ReactToastify

--- Gestor de Veículos/src/App.tsx
+++ Gestor de Veículos/src/App.tsx
@@ -1,10 +1,21 @@
+import { ToastContainer } from "react-toastify"
+import { VehicleProvider } from "./Context/VehicleContext"
+import AppRouter from "./routes"
+import BackgroundSlider from "./components/BackgroundSlider"
+
 
 
 function App() {
 
 
   return (
-    
+    <VehicleProvider>
+      <div className="z-[5]">
+        <BackgroundSlider />
+      </div>
+      <AppRouter />
+      <ToastContainer autoClose={1500} theme="colored" />
+    </VehicleProvider>
   )
 }
 
--- Gestor de Veículos/src/main.tsx
+++ Gestor de Veículos/src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './tailwind.css';
+import './tailwind.css'
 import App from './App.tsx'
+
+// Toastify
+import 'react-toastify/dist/ReactToastify.css'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <App />
   </StrictMode>,
 )
